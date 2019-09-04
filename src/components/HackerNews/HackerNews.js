f6c5fcda60f8a83ba52111e0692ae4f3867db78c
Grant <gwensignak@gmail.com>
2019-09-04 14:59:58 -0600

    completed

--- src/components/HackerNews/HackerNews.js
+++ src/components/HackerNews/HackerNews.js
@@ -1,26 +1,33 @@
-import React, { Component } from 'react';
-import Card from './../shared/Card/Card';
-import Loading from './../shared/Loading/Loading';
+import React, { Component } from 'react'
+import Card from './../shared/Card/Card'
+import Loading from './../shared/Loading/Loading'
+import { requestArticles } from '../../redux/hackerNewsReducer'
+import { connect } from 'react-redux'
 
 class HackerNews extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { articles: [], loading: true }
+  componentDidMount() {
+    this.props.requestArticles()
   }
 
   render() {
-    const articles = this.state.articles.map((article => <Card key={article.id} article={article} />))
+    const articles = this.props.articles.map((article) => (
+      <Card key={article.id} article={article} />
+    ))
     return (
       <div className='news-container'>
-        <img style={styles.logo} src="./hackerNews.jpeg" alt="" />
-        {this.state.loading ? <Loading /> : <div>{articles}</div>}
+        <img style={styles.logo} src='./hackerNews.jpeg' alt='' />
+        {this.props.loading ? <Loading /> : <div>{articles}</div>}
       </div>
     )
   }
 }
 
-export default HackerNews;
+function mapStateToProps(reduxState) {
+  console.log(state);
+  return state.hackerNews;
+}
 
+export default connect(mapStateToProps, { requestArticles })(HackerNews)
 
 const styles = {
   logo: {
